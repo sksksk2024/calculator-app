fa02a0d4c3872347100cb661621dcb85642fa1f4
Alex <cota8091@gmail.com>
2025-03-23 07:53:49 +0200

    Setup for the first theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Calculator App</title>
+        <meta
+          name="description"
+          content="Layout from where you can get inspiration"
+        />
+        <meta property="og:title" content="Calculator App" />
+        <meta property="og:description" content="Cool" />
+        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* General Meta Tags */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
